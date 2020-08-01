91aa159840690a359278eee926a857e5848f0ad5
bennettoh <bennettoh.arch@gmail.com>
2020-07-31 23:05:20 -0700

    basic board setup

---
+++ Assets/BoardManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    private const float TILE_SIZE = 1.0f;
+    private const float TILE_OFFSET = 0.5f;
+
+    // coordinates of selected pieces (-1 indicates none selected)
+    private int selectionX = -1;
+    private int selectionY = -1;
+
+    private void Update()
+    {
+        UpdateSelection();
+        DrawChessboard();
+    }
+
+    private void UpdateSelection()
+    {
+        if (!Camera.main) return;
+
+        // pick up where mouse is pointing and print coordinate
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("ChessPlane")))
+        {
+            Debug.Log(hit.point);
+        }
+    }
+
+    private void DrawChessboard()
+    {
+        Vector3 widthLine = Vector3.right * 8;
+        Vector3 heightLine = Vector3.forward * 8;
+
+        for(int i = 0; i <= 8; i++)
+        {
+            Vector3 start = Vector3.forward * i;
+            Debug.DrawLine(start, start + widthLine);
+            for (int j = 0; j <= 8; j++)
+            {
+                Vector3 height = Vector3.right * j;
+                Debug.DrawLine(height, height + heightLine);
+            }
+        }
+    }
+}
